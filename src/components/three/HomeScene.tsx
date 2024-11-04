2697cd243f488829da718c8af76ed3a1453f6af5
titouanlebocq <lebocq.titouan@gmail.com>
2024-11-04 18:59:19 +0100

    playing around with three js fiber

--- src/components/three/HomeScene.tsx
+++ src/components/three/HomeScene.tsx
@@ -1,28 +1,37 @@
 "use client"; // client side rendering
 
-import { Canvas } from "@react-three/fiber";
-import { useRef, useEffect } from "react";
+import styles from "./HomeScene.module.css";
+
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 import { Mesh } from "three";
 
 const Box = () => {
   const meshRef = useRef<Mesh>(null!);
 
-  useEffect(() => {
-    console.log(Boolean(meshRef.current));
-  }, []);
+  useFrame(({ clock }) => {
+    meshRef.current.rotation.x = clock.getElapsedTime();
+  });
 
   return (
     <mesh ref={meshRef}>
       <boxGeometry />
-      <meshBasicMaterial />
+      <meshBasicMaterial color="royalblue" />
     </mesh>
   );
 };
 
 export const HomeScene = () => {
   return (
-    <Canvas>
-      <Box />
-    </Canvas>
+    <div className={styles.scene}>
+      <Canvas>
+        <OrbitControls />
+        <PerspectiveCamera />
+        <Box />
+        <pointLight position={[10, 10, 10]} />
+        <gridHelper />
+      </Canvas>
+    </div>
   );
 };
